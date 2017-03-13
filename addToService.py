8df676389ba83675252deac916fd67d7f36af3c5
Allvin787 <vinni_vk@mail.ru>
2017-03-13 10:50:15 +0100

    Create addToService.py

---
+++ addToService.py
@@ -0,0 +1,12 @@
+import pypyodbc as pyodbc
+import random
+import pytest
+
+config = dict(
+    DRIVER='{SQL Server}',
+    server='10.200.7.42',
+    Trusted_connection='yes'
+   )
+
+cnx = pyodbc.connect(**config)
+cursor = cnx.cursor()

a98f4a7820826ddaa775336e33f4df6f8abece1d
Allvin787 <vinni_vk@mail.ru>
2017-03-15 14:33:17 +0100

    Update addToService.py

--- addToService.py
+++ addToService.py
@@ -14,7 +14,7 @@ try:
     cnx = pyodbc.connect(**config)
     cursor = cnx.cursor()
     print("SUCCESS")
-    sql = "SELECT * FROM [MODI_Technisat].[dbo].[TechnisatResults]"
+    sql = "SELECT * FROM [Alla_Testy].[dbo].[result]"
     print(sql)
     for row in cursor.execute(sql):
         print(row[6])

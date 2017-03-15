c5d56f0d696b907abdcd97f8327089b394b6c6e4
Allvin787 <vinni_vk@mail.ru>
2017-03-15 10:57:20 +0100

    Update addToService.py

--- addToService.py
+++ addToService.py
@@ -10,6 +10,10 @@ try:
     cs = 'DSN=%s;UID=%s;PWD=%s;DATABASE=%s;' % (host, username, password, database)
     cnxn = pyodbc.connect(cs)
     print ("SUCCESS")
+    sql = "SELECT * FROM [MODI_Technisat].[dbo].[TechnisatResults] where SnrBoard1 = '"+SnrBoard1+"'"
+    print(sql)
+    for row in cursor.execute(sql):
+        return row[6]
 except Exception as e:
     print ("Error: " + str(e))
 
